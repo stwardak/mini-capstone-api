d643dda6a746057f3bda2cfd391914f106b170dd
stwardak <148924249+stwardak@users.noreply.github.com>
2023-12-05 13:47:51 -0600

    implemented update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,13 @@ class ProductsController < ApplicationController
     render template: "products/show"
   end
 
-def update
-  
-
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render template: "products/show"
+  end
 end
