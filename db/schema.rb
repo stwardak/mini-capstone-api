1f88d5bd50db5b62648131e590800f3a1b492819
stwardak <148924249+stwardak@users.noreply.github.com>
2023-12-08 13:10:41 -0600

    changed price datatype to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_08_185942) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_190744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 10, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
