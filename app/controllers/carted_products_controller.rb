c0bd67e9c148f2e086b8b4bfe56442759a5eff69
stwardak <148924249+stwardak@users.noreply.github.com>
2024-01-10 12:21:42 -0600

    carted products index action to show only current user's cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,17 +1,24 @@
 class CartedProductsController < ApplicationController
   def index
-    @carted_products = CartedProduct.all 
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
     render :index
   end
 
   def create
+    
     @carted_product = CartedProduct.create(
-      user_id: params[:user_id],
+      # user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
-      order_id: params[:order_id],
-      status: params[:status],
+      quantity: params[:quantity,]
+      # order_id: params[:order_id],
+      order_id: nil,
+      # status: params[:status],
+      status: "carted",
     )
     render :show
   end
 
 end
+
+
--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,5 +1,5 @@
 class CartedProduct < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :user
-  has_many :products
+  belongs_to :product
 end
